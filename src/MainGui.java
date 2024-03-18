bb07154bf75f25cf17681f071a87c11c30c0e037
LEBEAULT Antoine <antoine.lebeault@etu.inp-n7.fr>
2024-03-18 13:38:30 +0100

    WIP Interface: experience layout

--- src/MainGui.java
+++ src/MainGui.java
@@ -1,6 +1,6 @@
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
@@ -13,20 +13,49 @@ public class MainGui {
 
     public MainGui() throws IOException {
         JFrame fenetre = new JFrame(NOM);
-        
-        JButton monButton = new JButton("Bouton 1");
-        fenetre.getContentPane().add(monButton);
-    
+        Container contenu = fenetre.getContentPane();
+        contenu.setLayout(new BorderLayout());
 
+        
+        // MENU DU HAUT
+        JPanel haut = new JPanel(new FlowLayout());
+        contenu.add(haut, BorderLayout.NORTH);
+        JLabel txt1 = new JLabel("Information 1");
+        haut.add(txt1);
+        JLabel txt2 = new JLabel("Information 2");
+        txt2.setFont(txt2.getFont().deriveFont(18f));
+        haut.add(txt2);
+        JLabel txt3 = new JLabel("Information 3");
+        haut.add(txt3);
+        
+        // MENU DU BAS
+        JPanel bas = new JPanel(new FlowLayout());
+        contenu.add(bas, BorderLayout.SOUTH);
+        JButton btn1 = new JButton("Bouton 1");
+        btn1.addActionListener(ev -> {
+            actionBouton();
+        });
+        bas.add(btn1);
+        JButton btn2 = new JButton("Bouton 2");
+        bas.add(btn2);
+        JButton btn3 = new JButton("Bouton 3");
+        bas.add(btn3);
+
+
+
+
+        // CENTRE
         BufferedImage myPicture = ImageIO.read(new File("../assets/board.jpg"));
         JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-        fenetre.getContentPane().add(picLabel);
+        contenu.add(picLabel, BorderLayout.CENTER);
     
         fenetre.pack();
         fenetre.setVisible(true);
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-
+    public void actionBouton() {
+        System.out.println("Bouton 1 cliqué !");
+    }
 
 }
