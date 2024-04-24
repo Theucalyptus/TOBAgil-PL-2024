b8320e88d9a06f4fbd7020e76d5180e3d020ec6a
Timothé LEBOBE <timothe.lebobe@gmail.com>
2024-04-24 14:06:14 +0200

    ajout dans le jeu

---
+++ src/jeu/testsUnitaires/TestJoueur.java
@@ -0,0 +1,18 @@
+package jeu.testsUnitaires;
+
+import org.junit.*;
+
+import jeu.Joueur;
+
+import static org.junit.Assert.*;
+
+/** Tester la classe Joueurs */
+public class TestJoueur {
+    
+    private Joueur unJoueur;
+
+    @Before
+    public void setUp() {
+        // TODO
+    }
+}
