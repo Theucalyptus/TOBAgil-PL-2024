385e4a75765b0e8cdcba9e36d2ab7068fd67afba
tle2734 <timothe.lebobe@etu.inp-n7.fr>
2024-04-25 16:29:37 +0200

    correction de quelques check styles

--- src/jeu/Jeu.java
+++ src/jeu/Jeu.java
@@ -1,8 +1,31 @@
 package jeu;
 
-/**Interface de modélisation du jeu SmallWorld */
+/**Interface de modélisation du jeu SmallWorld. */
 public interface Jeu {
-    
+
+    /**Nombre de Tour a jouer pour une partie de 2 joueurs. */
+    int NOMBRETOUR2JOUEURS = 10;
+
+
+    /**Nombre de Tour a jouer pour une partie de 3 joueurs. */
+    int NOMBRETOUR3JOUEURS = 9;
+
+
+    /**Nombre de Tour a jouer pour une partie de 4 ou 5 joueurs. */
+    int NOMBRETOUR45JOUEURS = 8;
+
+    /**Nombre de Joueurs possible dans une partie. */
+    int PARTIE2JOUEURS = 2;
+
+    /**Nombre de Joueurs possible dans une partie. */
+    int PARTIE3JOUEURS = 3;
+
+    /**Nombre de Joueurs possible dans une partie. */
+    int PARTIE4JOUEURS = 4;
+
+    /**Nombre de Joueurs possible dans une partie. */
+    int PARTIE5JOUEURS = 5;
+
     /**
      * Ajouter le joueur à la liste des joueurs.
      * @param joueur Le joueur qui rentre dans le jeu.
@@ -10,7 +33,7 @@ public interface Jeu {
     void ajouterJoueur(Joueur joueur);
 
     /**
-     * Renvoie le monde de la partie (la grille)
+     * Renvoie le monde de la partie (la grille).
      * @return le monde de la partie
      */
     Monde getMonde();
@@ -22,7 +45,8 @@ public interface Jeu {
     Joueur getJoueurCourant();
 
     /**
-     * Détermine le nombre de tour en fonction du nombre de joueur dans le 
+     * Détermine le nombre de tour en fonction du nombre de
+     * joueur dans le jeu.
      */
     void setNombreTour();
 }
