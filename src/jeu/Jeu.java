08ae1d234511894dc573807b578a501109ced9bc
Timothé LEBOBE <timothe.lebobe@gmail.com>
2024-04-26 22:48:20 +0200

    résolution pb de build

--- src/jeu/Jeu.java
+++ src/jeu/Jeu.java
@@ -12,4 +12,15 @@ public interface Jeu {
 
     /**Retirer tous les Joueurs de la listes des joueurs. */
     void reinitialiserJoueurs();
+
+
+    /**Obtenir le monde.
+     * @return Le monde de la partie.
+     */
+    Monde getMonde();
+
+    /** Permet d'obtenir le joueur qui est en train de jouer.
+     * @return Le Joueur courant.
+     */
+    Joueur getJoueurCourant();
 }
